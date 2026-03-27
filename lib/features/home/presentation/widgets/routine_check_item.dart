a0304555133354074e37b86a4574e470d77d1f44
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-27 12:13:11 +0900

    feat: 체크 컴폰너트 분리

--- lib/features/home/presentation/widgets/routine_check_item.dart
+++ lib/features/home/presentation/widgets/routine_check_item.dart
@@ -2,41 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:kdh_mobile/constants/color.dart';
 import 'package:kdh_mobile/constants/text_style.dart';
 import 'package:kdh_mobile/features/home/domain/entities/routine.dart';
-import 'package:material_symbols_icons/symbols.dart';
+import 'package:kdh_mobile/features/home/presentation/widgets/routine_check_circle.dart';
 
 class RoutineCheckItem extends StatelessWidget {
   const RoutineCheckItem({
     super.key,
     required this.routine,
     required this.isToday,
+    required this.onToggle,
     required this.onActionTap,
   });
 
   final Routine routine;
   final bool isToday;
+  final VoidCallback onToggle;
   final VoidCallback onActionTap;
 
   @override
   Widget build(BuildContext context) {
-    final isDone = routine.status == RoutineStatus.done;
-
-    final Color checkBgColor;
-    final Color checkIconColor;
-
-    if (isDone && isToday) {
-      checkBgColor = KdhColor.red200;
-      checkIconColor = KdhColor.background;
-    } else if (!isDone && isToday) {
-      checkBgColor = KdhColor.background;
-      checkIconColor = KdhColor.red100;
-    } else if (isDone && !isToday) {
-      checkBgColor = KdhColor.gray200;
-      checkIconColor = KdhColor.background;
-    } else {
-      checkBgColor = KdhColor.background;
-      checkIconColor = KdhColor.gray100;
-    }
-
     return Container(
       decoration: BoxDecoration(
         color: KdhColor.red50,
@@ -45,19 +28,10 @@ class RoutineCheckItem extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
       child: Row(
         children: [
-          Container(
-            width: 34,
-            height: 34,
-            decoration: BoxDecoration(
-              color: checkBgColor,
-              shape: BoxShape.circle,
-            ),
-            child: Icon(
-              Symbols.check_rounded,
-              color: checkIconColor,
-              size: 18,
-              fill: 1,
-            ),
+          RoutineCheckCircle(
+            isDone: routine.status == RoutineStatus.done,
+            isToday: isToday,
+            onToggle: onToggle,
           ),
           const SizedBox(width: 12),
           Expanded(
@@ -71,9 +45,7 @@ class RoutineCheckItem extends StatelessWidget {
                 const SizedBox(height: 2),
                 Text(
                   routine.subtitle,
-                  style: KdhTextStyle.caption1.copyWith(
-                    color: KdhColor.gray400,
-                  ),
+                  style: KdhTextStyle.caption1.copyWith(color: KdhColor.gray400),
                 ),
               ],
             ),
@@ -83,10 +55,7 @@ class RoutineCheckItem extends StatelessWidget {
             GestureDetector(
               onTap: onActionTap,
               child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 14,
-                  vertical: 8,
-                ),
+                padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
                 decoration: BoxDecoration(
                   color: KdhColor.red100,
                   borderRadius: BorderRadius.circular(50),

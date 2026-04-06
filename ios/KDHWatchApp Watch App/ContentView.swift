5939c476eedfcf643f152c11ad20cbec37a8e9e7
원이 <hawon9781@gmail.com>
2026-04-06 15:03:53 +0900

    feat: 스플래시 화면 Publishing

--- ios/KDHWatchApp Watch App/ContentView.swift
+++ ios/KDHWatchApp Watch App/ContentView.swift
@@ -10,9 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Spacer()
+
             Image("logo")
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal, 28)
+            Text("AI가 만드는 나만의 루틴")
+                .font(.kdf(.body1))
+                .foregroundStyle(.gray100)
+
+            Spacer()
         }
-        .padding()
+        .background(Color.background)
     }
 }
 

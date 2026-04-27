daf79d71ef91fac9a85c56f148cac656a306ec1c
원이 <hawon9781@gmail.com>
2026-04-27 11:03:24 +0900

    feat: 마이페이지 정보 퍼플리싱

--- ios/KDHWatchApp Watch App/Sources/My/MyView.swift
+++ ios/KDHWatchApp Watch App/Sources/My/MyView.swift
@@ -10,9 +10,19 @@ import SwiftUI
 struct MyView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Text("마이 페이지")
-                .font(.kdf(.body3))
-                .foregroundStyle(.gray800)
+            HStack(spacing: 12) {
+                Image("profile")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("하원")
+                        .foregroundStyle(Color.gray800)
+                        .font(.kdf(.caption1))
+                    Text("사용자 정보를 입력해주세요")
+                        .foregroundStyle(Color.gray300)
+                        .font(.kdf(.caption4))
+                }
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .background(Color.background)
